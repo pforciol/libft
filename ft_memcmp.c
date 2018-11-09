b9f8c2a9f0ef219069ca3ad278357b92cb9a79c1
Pierre FORCIOLI <pforciol@student.42.fr>
2018-11-09 10:56:27 +0100

    Improving ft_memchr.c and adding ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -1,29 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pforciol <pforciol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/09 10:32:11 by pforciol          #+#    #+#             */
-/*   Updated: 2018/11/09 10:49:00 by pforciol         ###   ########.fr       */
+/*   Created: 2018/11/09 10:40:04 by pforciol          #+#    #+#             */
+/*   Updated: 2018/11/09 10:55:12 by pforciol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t		i;
-	const char	*source;
+	const char	*temps1;
+	const char	*temps2;
 
-	source = s;
 	i = 0;
+	temps1 = s1;
+	temps2 = s2;
 	while (i < n)
 	{
-		if (source[i] == (unsigned char)c)
-			return (source[i]);
-		i++;
+		while ((temps1[i] != 0 && temps2[i] != 0) && temps1[i] == temps2[i])
+			i++;
+		return (temps1[i] - temps2[i]);
 	}
-	return (NULL);
 }
