1f95508df14eae05a8771cb9f68cda8d5142e10a
Pierre FORCIOLI <pforciol@student.42.fr>
2018-11-12 18:25:12 +0100

    Updating ft_strstr.c

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: pforciol <pforciol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 17:30:36 by pforciol          #+#    #+#             */
-/*   Updated: 2018/11/12 18:12:59 by pforciol         ###   ########.fr       */
+/*   Updated: 2018/11/12 18:24:25 by pforciol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,5 @@
 
 char	*ft_strstr(const char *haystack, const char *needle)
 {
-	int i;
-	int j;
-
-	i = 0;
-	if (ft_strlen(needle) == 0)
-		return ((char*)haystack);
-	while (haystack[i] != '\0')
-	{
-		j = 0;
-		while (haystack[i + j] == needle[j])
-		{
-			j++;
-			if (needle[j] == '\0')
-				return ((char*)haystack + i);
-		}
-		i++;
-	}
-	return (NULL);
+	return (ft_strnstr(haystack, needle, ft_strlen(haystack)));
 }
