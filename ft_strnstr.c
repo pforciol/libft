7f36acf905cbe99775448f0aec7319cc23e60e4e
Pierre FORCIOLI <pforciol@student.42.fr>
2018-11-12 18:22:30 +0100

    Adding ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pforciol <pforciol@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/12 18:15:15 by pforciol          #+#    #+#             */
+/*   Updated: 2018/11/12 18:21:57 by pforciol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t i;
+	size_t j;
+
+	i = 0;
+	if (ft_strlen(needle) == 0)
+		return ((char*)haystack);
+	while (haystack[i] != '\0' && i <= len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && (i + j) < len)
+		{
+			j++;
+			if (needle[j] == '\0')
+				return ((char*)haystack + i);
+		}
+		i++;
+	}
+	return (NULL);
+}
